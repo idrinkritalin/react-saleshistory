33474ed43411ff316b33c6b1b39e106bd66f2948
Stefano Veltri <stefano@idrinkritalin.it>
2018-06-22 18:05:03 +0200

    hello world

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3001;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Sales History app listening on port ${port}`);
+});
---
+++ src/App.spec.js
@@ -0,0 +1,5 @@
+describe('App', () => {
+  it('should do basic math', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
